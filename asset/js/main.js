d0d0f4e3896cbe31eb172f3071c50dc2beb27496
jazib iqbal <m.jazibiqbal@gmail.com>
2024-03-19 10:21:46 +0500

    abc

---
+++ asset/js/main.js
@@ -0,0 +1,18 @@
+// let btn_collasp = document.querySelector("#btn_collasp");
+// let popup_sect = document.querySelector("#popup_sect");
+// let body = document.querySelector("body")
+
+
+// btn_collasp.addEventListener('click', function(){
+//     popup_sect.classList.add("d_none")
+//     body.style.overflow = "auto";
+//     console.log(popup_sect.classList);
+
+// })
+
+
+
+// setTimeout(() => {
+//     popup_sect.style.display = "flex";
+//     body.style.overflow = "hidden"
+//   }, 3000);
